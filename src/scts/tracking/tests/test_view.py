c67a09872bfea4509bcb7f906359d4cfc11d3b19
Denis Capeto <deniscapeto@gmail.com>
2021-03-13 16:57:29 -0300

    Add test for view

---
+++ src/scts/tracking/tests/test_view.py
@@ -0,0 +1,71 @@
+import asyncio
+import json
+from unittest.mock import patch
+
+import pytest
+
+from scts.factory.build_app import build_app
+from scts.tracking.exceptions import CorreiosException
+
+
+class TestCorreiosTrackingView():
+
+    @pytest.fixture
+    def tracking_code(self):
+        return 'ON769530126BR'
+
+    @pytest.fixture
+    def client(self, aiohttp_client):
+        loop = asyncio.get_event_loop()
+        app = build_app()
+        return loop.run_until_complete(aiohttp_client(app))
+
+    @pytest.fixture
+    def mock_get_tracking_events(self, fake_html):
+        with patch(
+            'scts.tracking.views.CorreiosHttpClient.get_correios_tracking_events'
+        ) as mock:
+            mock.return_value = fake_html
+            yield mock
+
+    async def test_should_return_200_when_tracking_code_is_valid(
+        self,
+        client,
+        mock_get_tracking_events,
+        tracking_codes_dict,
+        tracking_code
+    ):
+
+        resp = await client.get(f'/tracking/{tracking_code}')
+
+        assert resp.status == 200
+        response_text = await resp.text()
+
+        assert json.loads(response_text) == tracking_codes_dict
+
+    async def test_should_return_200_when_correios_client_returned_error(
+        self,
+        client,
+        mock_get_tracking_events,
+        tracking_code
+    ):
+
+        mock_get_tracking_events.side_effect = CorreiosException
+
+        resp = await client.get(f'/tracking/{tracking_code}')
+
+        assert resp.status == 200
+        assert '{"Error": ["No donuts from Correios now =[", "Correios is unavailable"]}' == await resp.text()
+
+    async def test_should_return_500_when_correios_client_returned_unexpected(
+        self,
+        client,
+        mock_get_tracking_events,
+        tracking_code
+    ):
+
+        mock_get_tracking_events.side_effect = Exception
+
+        resp = await client.get(f'/tracking/{tracking_code}')
+
+        assert resp.status == 500
--- src/scts/tracking/views.py
+++ src/scts/tracking/views.py
@@ -2,6 +2,7 @@ import json
 
 from aiohttp import web
 
+from scts.tracking.exceptions import CorreiosException
 from scts.tracking.helpers import extract_tracking_events
 from scts.tracking.http_client import CorreiosHttpClient
 
@@ -12,8 +13,22 @@ class CorreiosTrackingView(web.View):
 
         tracking_code = self.request.match_info.get('tracking_code')
 
-        resp = await CorreiosHttpClient().get_correios_tracking_events(tracking_code)
+        try:
+            html_response = await CorreiosHttpClient().get_correios_tracking_events(tracking_code)
 
-        trackingCodes = extract_tracking_events(resp)
+        except CorreiosException:
+            error_body = json.dumps(
+                {
+                    'Error': [
+                        'No donuts from Correios now =[',
+                        'Correios is unavailable'
+                    ]
+                }
+            )
+            return web.Response(text=error_body, status=200)
+        except Exception:
+            return web.Response(status=500)
+
+        trackingCodes = extract_tracking_events(html_response)
 
         return web.Response(text=json.dumps(trackingCodes), status=200)
