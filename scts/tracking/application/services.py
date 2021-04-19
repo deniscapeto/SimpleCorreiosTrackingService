d6403aa82816127e99308158d17164f1ed321231
Denis Capeto <deniscapeto@gmail.com>
2021-04-18 21:38:09 -0300

    Upgrade django version to 3.2

--- scts/tracking/application/services.py
+++ scts/tracking/application/services.py
@@ -1,3 +1,5 @@
+from asgiref.sync import sync_to_async
+
 from scts.tracking.adapters.correios.http_client import get_correios_tracking_events
 
 
@@ -5,13 +7,14 @@ async def get_tracking_events(tracking_code: str):
 
     tracking_codes = await get_correios_tracking_events(tracking_code)
 
-    _save_tracking_code(tracking_codes)
+    await _save_tracking_code(tracking_codes)
 
     result = [tracking_code.as_dict() for tracking_code in tracking_codes]
 
     return result
 
 
+@sync_to_async
 def _save_tracking_code(tracking_codes):
     for t in tracking_codes:
         t.save()
