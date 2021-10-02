1059d3f5d241eee6f0db52cf6d9db8715580f3d0
Denis Capeto <deniscapeto@gmail.com>
2021-10-02 09:31:48 +0200

    Update django version from 3.2.4 to 3.2.5 due to security vulnerability

--- tests/unit/tracking/application/test_application_service.py
+++ tests/unit/tracking/application/test_application_service.py
@@ -1,35 +1,26 @@
-from unittest.mock import AsyncMock
+from unittest.mock import patch
 
 import inject
-import pytest
 
 from scts.tracking.application.services import GetTrackingEventsService
 
 
 class TestGetTrackingEventsService:
 
-    @pytest.fixture
-    def mock_get_tracking_events(
-        self,
-        tracking_events_list,
-    ):
-        service = inject.instance(GetTrackingEventsService)
-        service.get_correios_tracking_events = AsyncMock(
-            return_value=tracking_events_list
-        )
-
-        yield service
-
     async def test_should_return_tracking_events_when_valid_code_is_provided(
         self,
-        mock_get_tracking_events,
+        tracking_events_list,
         tracking_code,
     ):
         service = inject.instance(GetTrackingEventsService)()
         repository = service.trackingEventRepository
         repository_count_before = len(repository.tracking_events)
 
-        tracking_events = await service.get_tracking_events(tracking_code)
+        with patch(
+            'scts.tracking.application.services.get_correios_tracking_events', return_value=tracking_events_list
+        ):
+
+            tracking_events = await service.get_tracking_events(tracking_code)
 
         assert len(tracking_events) == 5
         assert len(repository.tracking_events) == repository_count_before + 5
